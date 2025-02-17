1e907166f441389bf31b1363ddcc4614f24a7b85
churchill <2cevening@gmail.com>
2025-02-17 08:50:16 +0800

    exercises

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import math
 
-x = 9
-y = 9.1
+#exercise1
 
-# print(math.pi)
-# print(math.e)  exponential
-# result = math.sqrt(x)  square root
-# result = math.ceil(y)
-result = math.floor(y)
+# radis = float(input("Enter the radius of a circle: "))
+#
+# area = math.pi * pow(radis, 2)
+# area = round(area, 2)
+#
+# print(f"The area of the circle is: {area}cm^2")
 
-print(result)
+
+#exercise2
+
+a = float(input("Enter side A:"))
+b = float(input("Enter side B:"))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(c)
